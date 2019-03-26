f05b16b99b94098a499d751a6b0a8a950be2e5ee
Luís Silva <lm2s@gmx.com>
2019-03-26 10:18:23 +0000

    Reverts to Lock

--- Observable/Classes/Observable.swift
+++ Observable/Classes/Observable.swift
@@ -1,42 +1,40 @@
 import Foundation
 
 public class ImmutableObservable<T> {
-
+    
     public typealias Observer = (T, T?) -> Void
-
+    
     private var observers: [Int: (Observer, DispatchQueue?)] = [:]
     private var uniqueID = (0...).makeIterator()
-
-    fileprivate let readWriteQueue: DispatchQueue = DispatchQueue(label: "Observable.ReadWriteQueue")
-    fileprivate let observersQueue: DispatchQueue = DispatchQueue(label: "Observable.ObserversQueue")
-
+    
+    fileprivate let lock: Lock = Mutex()
+    
     fileprivate var _value: T {
         didSet {
-            observersQueue.async {
-                self.observers.values.forEach { observer, dispatchQueue in
-                    if let dispatchQueue = dispatchQueue {
-                        dispatchQueue.async {
-                            observer(self.value, oldValue)
-                        }
-                    } else {
+            observers.values.forEach { observer, dispatchQueue in
+                if let dispatchQueue = dispatchQueue {
+                    dispatchQueue.async {
                         observer(self.value, oldValue)
                     }
+                } else {
+                    observer(value, oldValue)
                 }
             }
         }
     }
-
+    
     public var value: T {
-        return readWriteQueue.sync {
-            return _value
-        }
+        return _value
     }
-
+    
     public init(_ value: T) {
         self._value = value
     }
-
+    
     public func observe(_ queue: DispatchQueue? = nil, _ observer: @escaping Observer) -> Disposable {
+        lock.lock()
+        defer { lock.unlock() }
+        
         let id = uniqueID.next()!
         
         observers[id] = (observer, queue)
@@ -48,24 +46,22 @@ public class ImmutableObservable<T> {
         
         return disposable
     }
-
+    
     public func removeAllObservers() {
         observers.removeAll()
     }
 }
 
 public class Observable<T>: ImmutableObservable<T> {
-
+    
     public override var value: T {
         get {
-            return readWriteQueue.sync {
-                return _value
-            }
+            return _value
         }
         set {
-            readWriteQueue.async {
-                self._value = newValue
-            }
+            lock.lock()
+            defer { lock.unlock() }
+            _value = newValue
         }
     }
 }
