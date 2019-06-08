d6d72a099c94cbf190bb261f88d7610338fda37c
Robert-Hein Hooijmans <rh.hooijmans@gmail.com>
2019-06-08 15:23:44 +0200

    Add SPM support.

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "Observable",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "Observable", targets: ["Observable"])
+    ],
+    targets: [
+        .target(name: "Observable", path: "Observable/Classes")
+    ]
+)
